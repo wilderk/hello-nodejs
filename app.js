fdcc7b04e0d17cee689f1fc99da1337530623fa0
wilderk <wilderkevin@gmail.com>
2020-04-15 17:34:17 -0600

    switch to express

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const http = require('http');
-const url = require('url');
-const hostname = '127.0.0.1';
-const port = 8080;
+const express = require('express')
+const app = express()
+const port = 8080
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  pathName = url.parse(req.url).pathname
-  res.write('Hello ' + pathName);
-  res.end();
-});
+app.get('/', (req, res) => res.send('Hello Kevin!'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
